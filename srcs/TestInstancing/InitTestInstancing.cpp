e19f35942c15a7ef334f031955347b5cb28b3c58
cledant <cledant@student.42.fr>
2018-03-07 17:14:57 +0100

    added Alice model

--- srcs/TestInstancing/InitTestInstancing.cpp
+++ srcs/TestInstancing/InitTestInstancing.cpp
@@ -18,6 +18,7 @@ static void init_ressources(RessourceManager &rm)
 	rm.add_shader("BasicProp", "./shaders/BasicProp/BasicProp.vs",
 				  "./shaders/BasicProp/BasicProp.fs");
 	rm.add_model("Sakuya", "./assets/models/Sakuya/Sakuya_Izayoi.obj");
+	rm.add_model("Alice", "./assets/models/Alice/Alice.obj");
 }
 
 static void load_test_level(Glfw_manager &manager, RessourceManager &rm,
@@ -27,16 +28,16 @@ static void load_test_level(Glfw_manager &manager, RessourceManager &rm,
 								  glm::vec3(0.0f, 0.0f, 10.0f),
 								  glm::vec2(0.1f, 1000.0f), 60.0f, 10);
 
-	//Creating Sakuya RenderBin
-	ARenderBin::Params rb_sakuya;
-	rb_sakuya.shader       = &rm.getShader("BasicProp");
-	rb_sakuya.model        = &rm.getModel("Sakuya");
-	rb_sakuya.max_instance = 100000;
-	ARenderBin *sakuya = (*world)->add_RenderBin("Sakuya", rb_sakuya, ARenderBin::eType::PROP);
+	//Creating Model RenderBin
+	ARenderBin::Params rb_model;
+	rb_model.shader       = &rm.getShader("BasicProp");
+	rb_model.model        = &rm.getModel("Alice");
+	rb_model.max_instance = 100000;
+	ARenderBin *model = (*world)->add_RenderBin("Model", rb_model, ARenderBin::eType::PROP);
 
 	//Creating Prop
 	Prop::Params prop_params;
-	prop_params.render_bin = sakuya;
+	prop_params.render_bin  = model;
 	prop_params.orientation = glm::vec3(0.0f);
 	prop_params.scale       = glm::vec3(0.01f);
 	for (size_t i = 0; i < 50; ++i)
