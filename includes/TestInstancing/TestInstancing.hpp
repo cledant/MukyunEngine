3538d46b9c9e359559a3c06cb514af6375c5fe54
cledant <cledant@student.42.fr>
2018-03-07 14:21:36 +0100

    added class TestInstantacing

---
+++ includes/TestInstancing/TestInstancing.hpp
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   TestInstancing.hpp                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cledant <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/31 15:03:35 by cledant           #+#    #+#             */
+/*   Updated: 2017/09/23 17:30:23 by cledant          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TESTINSTANCING_HPP
+# define TESTINSTANCING_HPP
+
+# include "OpenGL/RessourceManager.hpp"
+# include "OpenGL/RenderBin/ARenderBin.hpp"
+# include "OpenGL/RenderBin/ClassesRenderBin/BasicPropRenderBin.hpp"
+# include "Exceptions/GeneralException.hpp"
+# include "glm/glm.hpp"
+# include "glm/gtc/matrix_transform.hpp"
+# include "Interfaces/IEntity.hpp"
+# include "GameEntities/Prop.hpp"
+# include "GameEntities/Utility/CollisionBox.hpp"
+# include "Cameras/Camera.hpp"
+# include "WindowManager/Window.hpp"
+# include "WindowManager/Input.hpp"
+# include <iostream>
+# include <vector>
+# include <fstream>
+# include <memory>
+# include <map>
+
+class TestInstancing
+{
+	public :
+
+		TestInstancing(Input const &input, GLFW_Window const &win,
+					   glm::vec3 const &cam_pos, glm::vec2 const &near_far,
+					   float max_fps, size_t max_frame_skip);
+		virtual ~TestInstancing(void);
+		TestInstancing(TestInstancing const &src) = delete;
+		TestInstancing &operator=(TestInstancing const &rhs) = delete;
+
+		/*
+		 * Draw
+		 */
+
+		void update(void);
+		void render(void);
+
+		/*
+		 * Object creation
+		 */
+
+		ARenderBin *add_RenderBin(std::string const &name, ARenderBin::Params &params,
+								  ARenderBin::eType type);
+		IEntity *add_Prop(Prop::Params &params);
+
+		/*
+		 * Other
+		 */
+
+		void updatePerspective(float fov);
+		void reset_update_timer(float time);
+		bool should_be_updated(float time);
+		void reset_skip_loop(void);
+
+		class TestInstancingFailException : public GeneralException
+		{
+			public :
+
+				explicit TestInstancingFailException(void);
+				virtual ~TestInstancingFailException(void) throw();
+		};
+
+	private :
+
+		std::map<std::string, std::unique_ptr<ARenderBin>> _render_bin_list;
+		std::vector<std::unique_ptr<IEntity>>              _entity_list;
+		GLFW_Window const                                  &_window;
+		glm::mat4                                          _perspective;
+		Camera                                             _camera;
+		glm::mat4                                          _perspec_mult_view;
+		float                                              _fov;
+		float                                              _max_fps;
+		size_t                                             _max_frame_skip;
+		float                                              _tick;
+		float                                              _next_update_tick;
+		float                                              _last_update_tick;
+		float                                              _delta_tick;
+		size_t                                             _skip_loop;
+		glm::vec2                                          _near_far;
+};
+
+#endif
---
+++ srcs/TestInstancing/TestInstancing.cpp
@@ -0,0 +1,130 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   TestInstancing.cpp                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cledant <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/04 16:34:42 by cledant           #+#    #+#             */
+/*   Updated: 2017/09/23 17:42:40 by cledant          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "TestInstancing/TestInstancing.hpp"
+
+TestInstancing::TestInstancing(Input const &input, GLFW_Window const &win,
+							   glm::vec3 const &cam_pos, glm::vec2 const &near_far,
+							   float max_fps, size_t max_frame_skip) :
+		_window(win), _camera(&input, cam_pos, glm::vec3(0.0f, 1.0f, 0.0f),
+							  glm::vec3(0.0f, 0.0f, -1.0f), -90.0f, 0.0f),
+		_fov(45.0f), _max_fps(max_fps), _max_frame_skip(max_frame_skip),
+		_next_update_tick(0.0f), _last_update_tick(0.0f), _delta_tick(0.0f),
+		_skip_loop(0), _near_far(near_far)
+{
+	if (max_frame_skip == 0)
+		throw TestInstancing::TestInstancingFailException();
+	GLfloat ratio = static_cast<GLfloat>(win.cur_win_w) /
+					static_cast<GLfloat>(win.cur_win_h);
+	this->_tick        = 1.0f / this->_max_fps;
+	this->_perspective = glm::perspective(glm::radians(this->_fov), ratio, near_far.x,
+										  near_far.y);
+}
+
+TestInstancing::~TestInstancing(void)
+{
+}
+
+/*
+ * Draw
+ */
+
+void TestInstancing::update(void)
+{
+	if (this->_window.resized)
+		this->updatePerspective(this->_fov);
+	this->_camera.update();
+	this->_perspec_mult_view = this->_perspective * this->_camera.getViewMatrix();
+	for (auto it = this->_render_bin_list.begin(); it != this->_render_bin_list.end(); ++it)
+		it->second.get()->flushMatrices();
+	for (auto it = this->_entity_list.begin(); it != this->_entity_list.end(); ++it)
+	{
+		it->get()->update(this->_tick);
+		it->get()->requestDraw();
+	}
+	for (auto it = this->_render_bin_list.begin(); it != this->_render_bin_list.end(); ++it)
+		it->second.get()->updateVBO();
+}
+
+void TestInstancing::render(void)
+{
+	for (auto it = this->_render_bin_list.begin(); it != this->_render_bin_list.end(); ++it)
+		it->second.get()->draw();
+}
+
+/*
+ * Object creation
+ */
+
+ARenderBin *TestInstancing::add_RenderBin(std::string const &name,
+										  ARenderBin::Params &params,
+										  ARenderBin::eType type)
+{
+	if (type == ARenderBin::eType::PROP)
+	{
+		this->_render_bin_list[name] = std::make_unique<BasicPropRenderBin>(params);
+		return (this->_render_bin_list[name].get());
+	}
+	return (nullptr);
+}
+
+IEntity *TestInstancing::add_Prop(Prop::Params &params)
+{
+	this->_entity_list.emplace_back(new Prop(params));
+	return (this->_entity_list.back().get());
+}
+
+/*
+ * Other
+ */
+
+void TestInstancing::updatePerspective(float fov)
+{
+	GLfloat ratio = static_cast<GLfloat>(this->_window.cur_win_w) /
+					static_cast<GLfloat>(this->_window.cur_win_h);
+	this->_perspective = glm::perspective(glm::radians(fov), ratio, this->_near_far.x,
+										  this->_near_far.y);
+}
+
+void TestInstancing::reset_update_timer(float time)
+{
+	this->_next_update_tick = time;
+	this->_last_update_tick = time;
+}
+
+void TestInstancing::reset_skip_loop(void)
+{
+	this->_skip_loop = 0;
+}
+
+bool TestInstancing::should_be_updated(float time)
+{
+	if (time > this->_next_update_tick &&
+		this->_skip_loop < this->_max_frame_skip)
+	{
+		(this->_skip_loop)++;
+		this->_next_update_tick += this->_tick;
+		this->_delta_tick       = time - this->_last_update_tick;
+		this->_last_update_tick = time;
+		return (true);
+	}
+	return (false);
+}
+
+TestInstancing::TestInstancingFailException::TestInstancingFailException(void)
+{
+	this->_msg = "TestInstancing : Something failed";
+}
+
+TestInstancing::TestInstancingFailException::~TestInstancingFailException(void) throw()
+{
+}
