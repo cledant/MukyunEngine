f6dc848106bd1eba637b02471ac1635501a28c4e
cledant <cledant@student.42.fr>
2018-03-08 13:48:08 +0100

    WIP designing light rendering

---
+++ includes/Light/ALight.hpp
@@ -0,0 +1,114 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ALight.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cledant <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/31 15:03:35 by cledant           #+#    #+#             */
+/*   Updated: 2017/09/23 17:30:23 by cledant          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ALIGHT_HPP
+# define ALIGHT_HPP
+
+# include "Interfaces/IEntity.hpp"
+# include "Interfaces/ITransformable.hpp"
+# include "OpenGL/RenderBin/ARenderBin.hpp"
+# include "glm/glm.hpp"
+# include "glm/gtc/matrix_transform.hpp"
+
+class ALight
+{
+	public :
+
+		enum eType
+		{
+			POINT,
+			DIRECTIONAL,
+			SPOT,
+		};
+
+		struct Params
+		{
+			Params(void);
+			~Params(void);
+
+			ARenderBin *model_rb;
+			ARenderBin *light_rb;
+			glm::vec3  ambient_color;
+			glm::vec3  diffuse_color;
+			glm::vec3  specular_color;
+			bool       draw_model;
+			bool       active;
+		};
+
+		ALight(void);
+		ALight(ALight::Params const &Params);
+		virtual ~ALight(void);
+		ALight(ALight const &src);
+		ALight &operator=(ALight const &rhs);
+
+
+		/*
+		 * Light
+		 */
+
+		virtual void addLight(void) = 0;
+
+		/*
+		 * Getter
+		 */
+
+		ALight::eType getLightType(void) const;
+		glm::vec3 const &getLightAmbientColor(void) const;
+		glm::vec3 const &getLightDiffuseColor(void) const;
+		glm::vec3 const &getLightSpecularColor(void) const;
+		glm::vec3 const &getDrawModel(void) const;
+
+		/*
+		 * Setter
+		 */
+
+		glm::vec3 const &setLightAmbientColor(void);
+		glm::vec3 const &setLightDiffuseColor(void);
+		glm::vec3 const &setLightSpecularColor(void);
+		glm::vec3 const &setDrawModel(void);
+
+		/*
+		 * Interface IEntity
+		 */
+
+		void update(float time);
+		void requestDraw(void);
+		void setActive(bool value);
+		bool getActive(void) const;
+
+		/*
+		 * Interface ITransformable
+		 */
+
+		void translateObject(glm::vec3 const &vec);
+		void scaleObject(glm::vec3 const &vec);
+		void rotateObject(glm::vec3 const &vec);
+
+	protected :
+
+		ARenderBin *_model_rb;
+		ARenderBin *_light_rb;
+		glm::vec3  _ambient_color;
+		glm::vec3  _diffuse_color;
+		glm::vec3  _specular_color;
+		bool       _draw_model;
+
+		//Related to IEntity
+		bool      _active;
+		glm::vec3 _model_pos;
+		glm::vec3 _model_scale;
+		//x = yaw; y = pitch; z = roll
+		glm::vec3 _model_orientation;
+		glm::mat4 _model;
+};
+
+#endif
---
+++ includes/Light/PointLight.hpp
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PointLight.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cledant <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/31 15:03:35 by cledant           #+#    #+#             */
+/*   Updated: 2017/09/23 17:30:23 by cledant          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef POINTLIGHT_HPP
+# define POINTLIGHT_HPP
+
+# include "Light/ALight.hpp"
+
+class PointLight : ALight
+{
+	public :
+
+		struct Params : ALight::Params
+		{
+			Params(void);
+			~Params(void);
+
+			glm::vec3 pos;
+			glm::vec3 attenuation_coeff;
+		};
+
+		PointLight(void);
+		virtual ~PointLight(void);
+		PointLight(const PointLight &src);
+		PointLight &operator=(PointLight const &rhs);
+
+		/*
+		 * Getter
+		 */
+
+		glm::vec3 const &getPos(void) const;
+		glm::vec3 const &getAttenuationCoeff(void) const;
+
+		/*
+		 * Interface IEntity
+		 */
+
+		virtual void update(float time);
+		virtual void requestDraw(void);
+		void setActive(bool value);
+		bool getActive(void) const;
+
+	protected :
+
+		glm::vec3 _pos;
+		//x = constant; y = linear; z = quadratic
+		glm::vec3 _attenuation_coeff;
+};
+
+#endif
