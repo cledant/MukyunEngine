01faa854546f50cf8fb236edb77d3c6bc9ff5faa
cledant <cledant@student.42.fr>
2018-04-08 12:24:09 +0200

    renamed executable + added missing Engine directories

---
+++ srcs/Engine/Init/InitTestInstancing.cpp
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   InitTestInstancing.cpp                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cledant <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/02 12:14:31 by cledant           #+#    #+#             */
+/*   Updated: 2017/09/24 13:31:34 by cledant          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "OpenGL/RessourceManager.hpp"
+#include "Engine/Engine.hpp"
+
+static void init_ressources(RessourceManager &rm)
+{
+	rm.add_texture("default_texture", "./assets/textures/default_texture/default_texture.tga",
+				   Texture::eTextureType::TEX_DIFFUSE_SPECULAR);
+	rm.add_model("Sakuya", "./assets/models/Sakuya/Sakuya_Izayoi.obj");
+	rm.add_model("Alice", "./assets/models/Alice/Alice.obj");
+}
+
+static void load_test_level(Glfw_manager &manager, RessourceManager &rm,
+							Engine **world)
+{
+	//Setting Shadow Renderer params
+	ShadowRenderer::Params sr_params;
+	sr_params.dir_depth_map_shader       = &rm.getShader("ComputeDirLightDepthMap");
+	sr_params.dir_shadow_map_shader      = &rm.getShader("ComputeDirShadowMaps");
+	sr_params.omni_depth_map_shader      = &rm.getShader("ComputeOmniDepthMap");
+	sr_params.omni_shadow_map_shader     = &rm.getShader("ComputeOmniShadowMaps");
+	sr_params.spot_dir_depth_map_shader  = &rm.getShader("ComputeDirLightDepthMap");
+	sr_params.spot_dir_shadow_map_shader = &rm.getShader("ComputeSpotLightShadowMaps");
+	sr_params.fuse_shadow_maps_shader    = &rm.getShader("DisplayImage");
+	sr_params.win_w                      = manager.getWindow().cur_win_w;
+	sr_params.win_h                      = manager.getWindow().cur_win_h;
+
+	(*world) = new Engine(manager.getInput(), manager.getWindow(),
+						  glm::vec3(0.0f, 0.0f, 10.0f),
+						  glm::vec2(0.1f, 1000.0f), 60.0f, 10,
+						  LightContainer::Params(), sr_params, rm);
+
+	//Creating Model RenderBin
+	ARenderBin::Params rb_model;
+	rb_model.shader       = &rm.getShader("BasicProp");
+	rb_model.model        = &rm.getModel("Alice");
+	rb_model.max_instance = 100000;
+	ARenderBin *model = (*world)->add_RenderBin("Model", rb_model, ARenderBin::eType::PROP);
+
+	//Creating Prop
+	Prop::Params prop_params;
+	prop_params.render_bin  = model;
+	prop_params.orientation = glm::vec3(0.0f);
+	prop_params.scale       = glm::vec3(0.01f);
+	for (size_t i = 0; i < 50; ++i)
+	{
+		for (size_t j = 0; j < 50; ++j)
+		{
+			prop_params.pos = glm::vec3(2.0f * i, 0.0f, 1.0f * j);
+			(*world)->add_Prop(prop_params);
+		}
+	}
+
+}
+
+static void init_program(Engine **world, RessourceManager &rm, Glfw_manager &manager)
+{
+	manager.create_window("TestInstancing", 4, 1, 1280, 720, true);
+	manager.displayGpuInfo();
+	manager.init_input_callback();
+	ShaderLoading(rm);
+	init_ressources(rm);
+	load_test_level(manager, rm, world);
+}
+
+void InitRunTestInstancing(Glfw_manager &manager)
+{
+
+	RessourceManager rm;
+	Engine           *world = nullptr;
+
+	try
+	{
+		init_program(&world, rm, manager);
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+		delete world;
+		return;
+	}
+	world->reset_update_timer(Glfw_manager::getTime());
+	manager.reset_fps_counter();
+	world->startGameLoop(manager);
+	std::cout << "Delete world" << std::endl;
+	delete world;
+}
---
+++ srcs/Engine/Init/ShaderLoading/ShaderLoading.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   InitTestDirectionalShadow.cpp                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cledant <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/02 12:14:31 by cledant           #+#    #+#             */
+/*   Updated: 2017/09/24 13:31:34 by cledant          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "OpenGL/RessourceManager.hpp"
+
+void ShaderLoading(RessourceManager &rm)
+{
+	rm.add_shader("BasicProp", "./shaders/BasicProp/BasicProp_vs.glsl",
+				  "./shaders/BasicProp/BasicProp_fs.glsl");
+	rm.add_shader("DiffuseColored", "./shaders/DiffuseColored/DiffuseColored_vs.glsl",
+				  "./shaders/DiffuseColored/DiffuseColored_fs.glsl");
+	rm.add_shader("MultiPointDirSpotLight", "./shaders/MultiPointDirSpotLight/MultiPointDirSpotLight_vs.glsl",
+				  "./shaders/MultiPointDirSpotLight/MultiPointDirSpotLight_fs.glsl");
+	rm.add_shader("ComputeDirLightDepthMap", "./shaders/ComputeDirLightDepthMap/ComputeDirLightDepthMap_vs.glsl",
+				  "./shaders/ComputeDirLightDepthMap/ComputeDirLightDepthMap_fs.glsl");
+	rm.add_shader("ComputeOmniDepthMap", "./shaders/ComputeOmniDepthMap/ComputeOmniDepthMap_vs.glsl",
+				  "./shaders/ComputeOmniDepthMap/ComputeOmniDepthMap_gs.glsl",
+				  "./shaders/ComputeOmniDepthMap/ComputeOmniDepthMap_fs.glsl");
+	rm.add_shader("ComputeDirShadowMaps", "./shaders/ComputeDirShadowMaps/ComputeDirShadowMaps_vs.glsl",
+				  "./shaders/ComputeDirShadowMaps/ComputeDirShadowMaps_fs.glsl");
+	rm.add_shader("ComputeOmniShadowMaps", "./shaders/ComputeOmniShadowMaps/ComputeOmniShadowMaps_vs.glsl",
+				  "./shaders/ComputeOmniShadowMaps/ComputeOmniShadowMaps_fs.glsl");
+	rm.add_shader("ComputeSpotLightShadowMaps",
+				  "./shaders/ComputeSpotLightShadowMaps/ComputeSpotLightShadowMaps_vs.glsl",
+				  "./shaders/ComputeSpotLightShadowMaps/ComputeSpotLightShadowMaps_fs.glsl");
+	rm.add_shader("DisplayDepthMap", "./shaders/DisplayDepthMap/DisplayDepthMap_vs.glsl",
+				  "./shaders/DisplayDepthMap/DisplayDepthMap_fs.glsl");
+	rm.add_shader("DisplayImage", "./shaders/DisplayImage/DisplayImage_vs.glsl",
+				  "./shaders/DisplayImage/DisplayImage_fs.glsl");
+	rm.add_shader("MultiPointDirSpotLightWithShadowMap",
+				  "./shaders/MultiPointDirSpotLightFusedWithShadowMap/MultiPointDirSpotLightFusedWithShadowMap_vs.glsl",
+				  "./shaders/MultiPointDirSpotLightFusedWithShadowMap/MultiPointDirSpotLightFusedWithShadowMap_fs.glsl");
+}
