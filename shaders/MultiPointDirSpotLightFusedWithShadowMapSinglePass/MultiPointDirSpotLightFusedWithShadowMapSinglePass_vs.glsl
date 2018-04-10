aef5b92e1525a38742357a2aec37a3d3594165d9
cledant <cledant@student.42.fr>
2018-04-10 19:27:50 +0200

    WIP singlepass light + shadow render

---
+++ shaders/MultiPointDirSpotLightFusedWithShadowMapSinglePass/MultiPointDirSpotLightFusedWithShadowMapSinglePass_vs.glsl
@@ -0,0 +1,39 @@
+#version 410 core
+
+layout (location = 0) in vec3 pos;
+layout (location = 1) in vec3 norm;
+layout (location = 2) in vec2 texCoord;
+layout (location = 3) in vec3 tangent;
+layout (location = 4) in vec3 biTangent;
+layout (location = 5) in mat4 instanceMatrix;
+layout (location = 9) in mat4 instanceInverseMatrix;
+
+out VS_OUT
+{
+    vec3 FragPos;
+    vec3 Normal;
+    vec2 Tex_coord;
+    vec4 FragPosLightSpace;
+} vs_out;
+
+uniform mat4	uniform_mat_perspec_mult_view;
+uniform mat4	uniform_lightSpaceMatrix;
+uniform int		type;
+
+/*
+ * Type List :
+ *
+ * 		Omnidirectional = 0
+ * 		Directional = 1
+ *		Spot = 2
+ */
+
+void main()
+{
+    vs_out.FragPos = vec3(instanceMatrix * vec4(pos, 1.0));
+    vs_out.Normal = mat3(instanceInverseMatrix) * norm;
+    vs_out.TexCoords = texCoord;
+	if (type > 0)
+	    vs_out.FragPosLightSpace = uniform_lightSpaceMatrix * vec4(vs_out.FragPos, 1.0);
+    gl_Position = uniform_mat_perspec_mult_view * vec4(vs_out.FragPos, 1.0);
+}
